26ca2a71b6d5ddc1a195d7e9606adc40e5c743d5
Bobbert Banana <hanuman.bheema@gmail.com>
2022-05-30 04:52:32 +0100

    final RedBlackTree

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //Nihal Parthasrathy
-//5/7/2022
-//This code makes a red black tree to sort numbers in a balanced form. It adds the numbers form manual adding or from a file and also displays it
+//5/29/2022
+//This code makes a red black tree to sort numbers in a balanced form. It adds the numbers form manual adding or from a file and also displays it. It also removes from the tree and you can search for something in it
 
 //Inclusions
 #include "node.h"
@@ -315,29 +315,22 @@ void move(Node* &root, Node* curr) {
   }
 }
 
-void checkDelete(Node* curr, Node* &root) {
-  cout << "check delete called" << endl;
+void checkDelete(Node* curr, Node* &root) {//Check delete function which has cases for double blacks
   Node* sibling = NULL;
-  //Node* siblingLeft = NULL;
-  //Node* siblingRight = NULL;
   if (curr == root) {//CASE 1
-    cout << "case 1" << endl;
     return;
   }
   else {
-    cout << curr->data << endl;
+    //Sets up varubles
     Node* parent = curr->parent;
     if (curr == curr->parent->getRight()) {
       sibling = curr->parent->getLeft();
-      cout << "sibling is left" << endl;
     }
     else if (curr == curr->parent->getLeft()) {
       sibling = curr->parent->getRight();
-      cout << "sibling is right" << endl;
     }
-    cout << "hello" << endl;
     //CASE 2
-    if (sibling->color == true && curr == curr->parent->getLeft()) {
+    if (sibling->color == true && curr == curr->parent->getLeft() && curr->color == false && parent->color == false) {//Rotates the sibling through the parent
       Node* siblingLeft = sibling->getLeft();
       display(root, 0);
       sibling->parent = parent->parent;
@@ -353,23 +346,17 @@ void checkDelete(Node* curr, Node* &root) {
         root = sibling;
       }
       sibling->setLeft(parent);
-      cout << "break" << endl;
       parent->parent = sibling;
-      parent->color = true;
+      parent->color = true;//changes colors
       sibling->color = false;
       parent->setRight(siblingLeft);
       if (siblingLeft != NULL) {
         siblingLeft->parent = parent;
       }
-      display(root, 0);
-      Node* temporary = sibling;
-      sibling = sibling->getLeft();
-      parent = temporary;
-      cout << "case 2" << endl;
+      sibling = parent->getRight();//Resets sibling
     }
-    else if (sibling->color == true && curr == curr->parent->getRight()) {
+    else if (sibling->color == true && curr == curr->parent->getRight() && curr->color == false && parent->color == false) {//Rotates sibling through parent
       Node* siblingRight = sibling->getRight();
-      display(root, 0);
       sibling->parent = parent->parent;
       if (parent != root) {
 	if (parent == parent->getLeft()) {
@@ -383,41 +370,34 @@ void checkDelete(Node* curr, Node* &root) {
 	root = sibling;
       }
       sibling->setRight(parent);
-      cout << "break" << endl;
       parent->parent = sibling;
-      parent->color = true;
+      parent->color = true;//Changes colors
       sibling->color = false;
       parent->setLeft(siblingRight);
       if (siblingRight != NULL) {
 	siblingRight->parent = parent;
       }
-      display(root, 0);
       Node* temporary = sibling;
-      sibling = sibling->getRight();
-      parent = temporary;
-      cout << "case 2" << endl;
+      sibling = parent->getLeft();//Resets sibling
     }
 
     
     //CASE 3
-    if (sibling->color == false) {
+    if (sibling->color == false && curr->color == false && parent->color == false && (sibling->getLeft() == NULL || sibling->getLeft()->color == false) && (sibling->getRight() == NULL || sibling->getRight()->color == false)) {//Sets siblings color to red then recurcive call on parent
       sibling->color = true;
-      cout << "case 3" << endl;
       checkDelete(parent, root);
     }
     //CASE 4
-    else if (parent->color == true && sibling->color == false) {
-      if (((sibling->getLeft() == NULL || sibling->getLeft()->color == false) && (sibling->getRight() == NULL || sibling->getRight()->color == false))) {
-	parent->color = false;
-	sibling->color = true;
-	cout << "case 4" << endl;
-      }
+    else if (parent->color == true && sibling->color == false && (sibling->getLeft() == NULL || sibling->getLeft()->color == false) && (sibling->getRight() == NULL || sibling->getRight()->color == false)) {//Sets parent to black and sibling to red
+      parent->color = false;
+      sibling->color = true;
+      return;
     }
     //CASE 5
-    else if (parent->getLeft() == sibling && (sibling->getLeft() == NULL || sibling->getLeft()->color == false)) {
+    else if (parent->getLeft() == sibling && (sibling->getLeft() == NULL || sibling->getLeft()->color == false)) {//Rotates through sibling
       if (sibling->getRight() != NULL) {
-	if (sibling->getRight()->color == true) {
-	  Node* siblingRight = sibling->getRight();
+	if (sibling->getRight()->color == true) {//If siblings right is red
+	  Node* siblingRight = sibling->getRight();//Rotates trhough sibling
 	  parent->setLeft(siblingRight);
 	  siblingRight->parent = parent;
 	  Node* temp = siblingRight->getLeft();
@@ -425,16 +405,16 @@ void checkDelete(Node* curr, Node* &root) {
 	  sibling->parent = siblingRight;
 	  sibling->setRight(temp);
 	  temp->parent = sibling;
-	  sibling->color = true;
+	  sibling->color = true;//Sets colors
 	  siblingRight->color = false;
-	  cout << "case 5" << endl;
+	  sibling = siblingRight;//Resets sibling
 	}
       }
     }
     else if (parent->getRight() == sibling && (sibling->getRight() == NULL || sibling->getRight()->color == false)) {
       if (sibling->getLeft() != NULL) {
         if (sibling->getLeft()->color == true) {
-	  Node* siblingLeft = sibling->getLeft();
+	  Node* siblingLeft = sibling->getLeft();//Rotates through sibling
           parent->setRight(siblingLeft);
           siblingLeft->parent = parent;
           Node* temp = siblingLeft->getRight();
@@ -442,18 +422,18 @@ void checkDelete(Node* curr, Node* &root) {
           sibling->parent = siblingLeft;
           sibling->setLeft(temp);
           temp->parent = sibling;
-          sibling->color = true;
+          sibling->color = true;//Sets colors
           siblingLeft->color = false;
-	  cout << "case 5" << endl;
+	  sibling = siblingLeft;//Resets sibling
         }
       }
     }
     //CASE 6
-    else if (sibling->color == false && parent->getLeft() == sibling && sibling->getLeft() != NULL) {
+    if (sibling->color == false && parent->getLeft() == sibling && sibling->getLeft() != NULL && curr->color == false) {//Rotates through parent and switchs parent and siblings colors
       if (sibling->getLeft()->color == true) {
 	Node* temp = sibling->getRight();
 	sibling->setRight(parent);
-	if (parent->parent != NULL) {
+	if (parent->parent != NULL) {//Checks if parent is NULL
 	  if (parent == parent->parent->getRight()) {
 	    parent->parent->setRight(sibling);
 	    sibling->parent = parent->parent;
@@ -469,14 +449,17 @@ void checkDelete(Node* curr, Node* &root) {
 	}
 	parent->parent = sibling;
 	parent->setLeft(temp);
-	temp->parent = parent;
+	if (temp != NULL) {
+	  temp->parent = parent;
+	}
+	//Sets colors
 	sibling->getLeft()->color = false;
 	sibling->color = parent->color;
 	parent->color = false;
-	cout << "case 6" << endl;
+	return;
       }
     }
-    else if (sibling->color == false && parent->getRight() == sibling && sibling->getLeft() != NULL) {
+    else if (sibling->color == false && parent->getRight() == sibling && sibling->getRight() != NULL && curr->color == false) {
       if (sibling->getRight()->color == true) {
 	Node* temp = sibling->getLeft();
         sibling->setLeft(parent);
@@ -496,11 +479,14 @@ void checkDelete(Node* curr, Node* &root) {
         }
 	parent->parent = sibling;
 	parent->setRight(temp);
-	temp->parent = parent;
+	if (temp != NULL) {
+	  temp->parent = parent;
+	}
+	//Sets colors
 	sibling->getRight()->color = false;
 	sibling->color = parent->color;
 	parent->color = false;
-	cout << "case 6" << endl;
+	return;
       }
     }
   }
@@ -531,21 +517,14 @@ void remove(Node* &root, Node* curr, int num, Node* newptr) {//Removes the numbe
       }
       else {//If it has two children
 	Node* newNode = temp->right;
-        while (newNode->left != NULL) {
+        while (newNode->left != NULL) {//Goes to inorder sucser
           newNode = newNode->left;
         }
         temp->data = newNode->data;
-	checkDelete(newNode, root);
+	checkDelete(newNode, root);//Calls check delete
         if (newNode == temp->getRight()) {
           temp->right = temp->right->right;
-	  //temp->right->right->parent = temp;
         }
-	/*else {
-	  newNode->parent->setLeft(newNode->getRight());
-	  if (newNode->getRight() != NULL) {
-	    newNode->getRight()->parent = newNode->parent;
-	  }
-	  }*/
 	else if(newNode->getRight() != NULL){
 	  newNode->parent->left = newNode->right;
 	  if(newNode->parent->color){
@@ -560,15 +539,7 @@ void remove(Node* &root, Node* curr, int num, Node* newptr) {//Removes the numbe
       }
     }
     else {
-      if (temp->getLeft() == NULL && temp->getRight() == NULL) {//If the it is a leaf
-	/*if (temp->parent->getLeft() == temp) {//If temp is a left child of its parents delete it and make its parents left point to null
-	  temp->parent->left = NULL;
-	  delete temp;
-	}
-	else {//If right child
-	  temp->parent->right = NULL;
-	  delete temp;
-	  }*/
+      if (temp->getLeft() == NULL && temp->getRight() == NULL) {//If it is a leaf
 	if (temp->color == true) {
 	  if (temp->parent->getLeft() == temp) {
 	    temp->parent->setLeft(NULL);
@@ -580,11 +551,11 @@ void remove(Node* &root, Node* curr, int num, Node* newptr) {//Removes the numbe
 	  }
 	  return;
 	}
-	if (temp->parent->getLeft() == temp) {
+	if (temp->parent->getLeft() == temp) {//Calls check delete then deletes
 	  checkDelete(temp, root);
 	  temp->parent->setLeft(NULL);
 	}
-	else {
+	else {//Calls check delete then delets
 	  checkDelete(temp, root);
 	  temp->parent->setRight(NULL);
 	}
@@ -594,88 +565,91 @@ void remove(Node* &root, Node* curr, int num, Node* newptr) {//Removes the numbe
 	//MAKEs THE PARENTS POINTER POINT TO THE CHILD (EASIER)
 	Node* tempParent = temp->parent;
 	if (tempParent->getLeft() == temp) {
-	  if (temp->left->color == !temp->color) {
-	    delete temp;
+	  if (temp->left->color == !temp->color) {//If colors are diffrent
 	    tempParent->setLeft(temp->left);
-	    if (temp->left->color) {
-	      temp->color = false;
+	    temp->left->parent = tempParent;
+	    if (temp->left->color) {//Sets temps left new color based on what the color variations are
+	      temp->left->color = false;
 	    }
+	    else {
+	      temp->left->color = true;
+	    }
+	    delete temp;
 	    return;
 	  }
+	  //Deletes temp
 	  tempParent->setLeft(temp->left);
-	  checkDelete(temp, root);
+	  temp->left->parent = tempParent;
+	  delete temp;
 	}
 	else {
-	  if (temp->left->color == !temp->color) {
-	    delete temp;
-	    tempParent->right = temp->left;
-	    if (temp->left->color) {
-	      temp->color = false;
+	  if (temp->left->color == !temp->color) {//If the colors are diffrent
+	    tempParent->setRight(temp->left);
+	    temp->left->parent = tempParent;
+	    if (temp->left->color) {//Sets temps left new color based on the diffre color variations
+	      temp->left->color = false;
 	    }
+	    else {
+	      temp->left->color = true;
+	    }
+	    delete temp;
 	    return;
 	  }
-	  tempParent->right = temp->left;
+	  //Deletes temp
+	  tempParent->setRight(temp->left);
+	  temp->left->parent = tempParent;
 	  checkDelete(temp, root);
 	}
-	/*if (tempParent->getLeft() == temp) {//If it is a left child
-	  //move(root, temp);
-	  tempParent->left = temp->getLeft();
-	  temp->getLeft()->parent = tempParent;
-	}
-	else {//If it is a right child
-	  //move(root, temp);
-	  tempParent->right = temp->getLeft();
-	  temp->getLeft()->parent = tempParent;
-	}
-	delete temp;//Deletes temp*/
 	newptr = NULL;
       }
       else if (temp->getLeft() == NULL && temp->getRight() != NULL) {//If temp only has a right child
 	Node* tempParent = temp->parent;
-	/*if (tempParent->getLeft() == temp) {//If it is a right child
-	  //move(root, temp);
-	  tempParent->left = temp->getRight();
-	  temp->getRight()->parent = tempParent;
-	}
-	else {//If it is a right child
-	  //move(root, temp);
-	  tempParent->right = temp->getRight();
-	  temp->getRight()->parent = tempParent;
-      }
-      delete temp;*/
 	if (tempParent->getRight() == temp) {
-          if (temp->right->color == !temp->color) {
-            delete temp;
+          if (temp->right->color == !temp->color) {//If colors are diffrent
             tempParent->setRight(temp->right);
-            if (temp->left->color) {
-              temp->color = false;
+	    temp->right->parent = tempParent;
+            if (temp->right->color) {//Sets temp left based off color varients
+              temp->right->color = false;
             }
+	    else {
+	      temp->right->color = true;
+	    }
+	    delete temp;
             return;
           }
+	  //deletes temp then runs check delete
           tempParent->setRight(temp->right);
+	  temp->right->parent = tempParent;
+	  delete temp;
           checkDelete(temp, root);
-        }
-        else {
-          if (temp->right->color == !temp->color) {
-            delete temp;
-            tempParent->left = temp->right;
+	}
+	else {
+	  if (temp->right->color == !temp->color) {
+	    tempParent->setLeft(temp->right);
+	    temp->right->parent = tempParent;
             if (temp->right->color) {
-              temp->color = false;
+              temp->right->color = false;
             }
+	    else {
+	      temp->right->color = true;
+	    }
+	    delete temp;
             return;
-          }
-          tempParent->left = temp->right;
-          checkDelete(temp, root);
-        }
+	  }
+	  tempParent->setLeft(temp->right);
+	  temp->right->parent = tempParent;
+	  delete temp;
+	  checkDelete(temp, root);
+	}
 	newptr = NULL;
       }
       else {//If it has two children
 	Node* newNode = temp->right;
-	while (newNode->left != NULL) {
+	while (newNode->left != NULL) {//Finds in order sucser
 	  newNode = newNode->left;
 	}
 	temp->data = newNode->data;
-	checkDelete(newNode, root);
+	checkDelete(newNode, root);//Checks
 	if (newNode == temp->getRight()) {
 	  temp->right = temp->right->right;
 	}
@@ -686,4 +660,5 @@ void remove(Node* &root, Node* curr, int num, Node* newptr) {//Removes the numbe
     }
   }
 }
-    
+
+
